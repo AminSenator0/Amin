75deaa770c2f81aafae435da21f2594eaa04e1e9
AminSenator0 <lonabehrozi@gmail.com>
2025-05-06 15:17:29 +0330

    fix: use renderToPipeableStream instead of renderToReadableStream

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,55 +1,43 @@
-import type { AppLoadContext, EntryContext } from "@remix-run/cloudflare";
+import { PassThrough } from "stream";
 import { RemixServer } from "@remix-run/react";
-import * as isbotModule from "isbot";
-import { renderToReadableStream } from "react-dom/server";
+import ReactDOMServer from "react-dom/server";
+import type { EntryContext } from "@remix-run/node";
 
-export default async function handleRequest(
+// مهم: destructure از default import
+const { renderToPipeableStream } = ReactDOMServer;
+
+export default function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
-  remixContext: EntryContext,
-  loadContext: AppLoadContext
+  remixContext: EntryContext
 ) {
-  const body = await renderToReadableStream(
-    <RemixServer context={remixContext} url={request.url} />,
-    {
-      signal: request.signal,
-      onError(error: unknown) {
-        // Log streaming rendering errors from inside the shell
-        console.error(error);
-        responseStatusCode = 500;
-      },
-    }
-  );
+  return new Promise((resolve, reject) => {
+    let didError = false;
 
-  if (isBotRequest(request.headers.get("user-agent"))) {
-    await body.allReady;
-  }
+    const stream = new PassThrough();
 
-  responseHeaders.set("Content-Type", "text/html");
-  return new Response(body, {
-    headers: responseHeaders,
-    status: responseStatusCode,
+    const { pipe } = renderToPipeableStream(
+      <RemixServer context={remixContext} url={request.url} />,
+      {
+        onShellReady() {
+          responseHeaders.set("Content-Type", "text/html");
+          resolve(
+            new Response(stream, {
+              status: didError ? 500 : responseStatusCode,
+              headers: responseHeaders,
+            })
+          );
+          pipe(stream);
+        },
+        onShellError(err) {
+          reject(err);
+        },
+        onError(err) {
+          didError = true;
+          console.error(err);
+        },
+      }
+    );
   });
-}
-
-// We have some Remix apps in the wild already running with isbot@3 so we need
-// to maintain backwards compatibility even though we want new apps to use
-// isbot@4.  That way, we can ship this as a minor Semver update to @remix-run/dev.
-function isBotRequest(userAgent: string | null) {
-  if (!userAgent) {
-    return false;
-  }
-
-  // isbot >= 3.8.0, >4
-  if ("isbot" in isbotModule && typeof isbotModule.isbot === "function") {
-    return isbotModule.isbot(userAgent);
-  }
-
-  // isbot < 3.8.0
-  if ("default" in isbotModule && typeof isbotModule.default === "function") {
-    return isbotModule.default(userAgent);
-  }
-
-  return false;
 }
